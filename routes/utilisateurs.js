a8f2f9531673ae2bb55225c206d6ed9a28416ea1
Nathan Lamy <e2036136@cmaisonneuve.qc.ca>
2022-09-17 19:27:45 -0400

    Update + get user with id
    - Updated post inscription
    - Si email existe deja -> erreur 500
    - Created get user with id

--- routes/utilisateurs.js
+++ routes/utilisateurs.js
@@ -6,6 +6,8 @@ const chaineConnexion = require('../constantes');
 
 const router = express.Router();
 
+require ('mssql/msnodesqlv8')
+
 router.get('/', async (req, res) => {
     chaineConnexion.connect().then(() => {
         //simple query
@@ -17,40 +19,44 @@ router.get('/', async (req, res) => {
 
 });
 
-require ('mssql/msnodesqlv8')
+router.get('/:id', async (req, res) => {
+    chaineConnexion.connect().then(() => {
+        //simple query
+        chaineConnexion.request().query(`select * from utilisateurs where id = ${req.params.id}`, (err, result) => {
+              console.log(result.recordsets[0])
+              return res.status(200).json(result.recordsets[0]);
+        })
+      })
 
+});
 
 router.post('/inscription', async (req, res) => {
     let bodyy = {...req.body}
     console.log(bodyy);
+
     bodyy.MotDePasse = await bcrypt.hash(bodyy.MotDePasse, 8);
 
     var now = new Date();
     var datenow = now.toISOString();
     var NULL = null
 
-    try {
-        chaineConnexion.connect().then(() => {
+    chaineConnexion.connect().then(() => {
             chaineConnexion.query(`insert into Utilisateurs values
-            ('${bodyy.Prenom}',
-            '${bodyy.NomDeFamille}',
-            '${bodyy.Courriel}',
-            '${bodyy.MotDePasse}',
-            '${datenow}',
-            '${datenow}',
-            '${NULL}',
-            '${NULL}')`, (err) => {
-                if(err) {
-                    console.log(err);
-                    return res.status(500).send("Server error!!!!");
-                }
-            });
-        })
-    }
-    catch (err) {
-        console.log(err);
-    }
-    return res.status(200).send({Success: bodyy});
+        ('${bodyy.Prenom}',
+        '${bodyy.NomDeFamille}',
+        '${bodyy.Courriel}',
+        '${bodyy.MotDePasse}',
+        '${datenow}',
+        '${datenow}',
+        '${NULL}',
+        '${NULL}')`, (err) => {
+            if(err) {
+                return res.status(500).send("Le courriel existe deja");
+            } else {
+                return res.status(200).send({Success: bodyy});
+            }
+        });
+    })
 });
 
 module.exports = router;
