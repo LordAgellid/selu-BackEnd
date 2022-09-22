f4edf9e4f5b572f626fe68d69837e6cd1ce70c2d
Nathan Lamy <e2036136@cmaisonneuve.qc.ca>
2022-09-22 16:19:45 -0400

    https
    Ajout de Certificat SSL 
    https local host

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
+
 const app = express();
 const cors = require('cors');
 
@@ -13,6 +17,13 @@ app.use(express.json());
 
 app.use('/utilisateurs', utilisateursRouter);
 
-app.listen(PORT, () => {
-    console.log(`Mon application roule sur http://localhost:${PORT}`);
-});
+const sslServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+}, app)
+
+sslServer.listen(PORT, () => console.log(`Mon application roule sur https://localhost:${PORT}`));
+
+// app.listen(PORT, () => {
+//     console.log(`Mon application roule sur http://localhost:${PORT}`);
+// });
