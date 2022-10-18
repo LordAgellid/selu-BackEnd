fcd4f2a4d5f49f4f31f8c3a04122e5c6b988e532
Yao Kounakou <e1955810@cmaisonneuve.qc.ca>
2022-10-17 22:26:42 -0400

    Correction du middleware Connexion

--- routes/connexion.js
+++ routes/connexion.js
@@ -6,19 +6,6 @@ const request = require('../database/utilisateurs');
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-
-    let resultat;
-    try {
-        resultat = await request.getUtilisateursAll();
-    } catch (error) {
-        res.status(500).json(error.message);
-    }
-
-    return res.status(200).json(resultat);
-});
-
 router.post('/', async (req, res) => {
     res.header('Access-Control-Allow-Origin', '*');
 
